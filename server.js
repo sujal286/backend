70366e42d65df58ba32575f28209ae5ef10f06e5
Sujal Saha <sujalsaha14@gmail.com>
2025-02-07 00:54:32 +0530

    i

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const app = express();
 
 
 // Middleware
-app.use(cors({ 
-  origin: 'https://frontend-seven-teal-38.vercel.app', 
-  credentials: true 
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
