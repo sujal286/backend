ade02cf2a16a11d713e9dc4ebffe6a74b7f57b7b
Sujal Saha <sujalsaha14@gmail.com>
2025-02-07 00:29:49 +0530

    eww

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000;
+
 
 // Middleware
-app.use(cors());
+app.use(cors({ 
+  origin: 'https://frontend-seven-teal-38.vercel.app', 
+  credentials: true 
+}));
 app.use(bodyParser.json());
 
 // MongoDB connection
@@ -61,7 +64,8 @@ app.post('/contact', async (req, res) => {
   }
 });
 
+const PORT =  5000;
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
